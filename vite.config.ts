0aade67212e8fbb750520731a0f6b7475e0c24bc
mengyu <mengyu822@aliyun.com>
2024-01-27 18:40:06 +0800

    修改地址

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,8 @@ export default defineConfig({
     proxy: {
       // 选项写法
       '/api': {
-        target: 'http://localhost:5000',
+        // target: 'http://localhost:5000',
+        target: 'https://mengyu822.github.io',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
