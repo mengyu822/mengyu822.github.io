2a0cbe46af5bdc326aa1a6c46b47bf2bd5e25ef0
mengyu <mengyu822@aliyun.com>
2024-01-27 22:27:10 +0800

    1，实现分页器功能 2，添加hintrc测试

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
       '/api': {
         // target: 'http://localhost:5000',
         target: 'https://mengyu822.github.io',
+        
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
